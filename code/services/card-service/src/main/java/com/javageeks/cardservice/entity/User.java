bbb86aa6ae812131d68e1c65998ed9024d32941a
chidananda pati <chidananda.pati@sjsu.edu>
2018-07-22 10:42:26 -0700

    moved to services

---
+++ code/services/card-service/src/main/java/com/javageeks/cardservice/entity/User.java
@@ -0,0 +1,21 @@
+package com.javageeks.cardservice.entity;
+
+public class User {
+	private Long id;
+	private String userName;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
